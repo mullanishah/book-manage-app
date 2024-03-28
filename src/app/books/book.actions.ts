fdeeff0f3b4595282c50784ed98e628df7d7cd37
mullanishah <mullanishah@gmail.com>
2024-03-28 15:18:26 +0530

    ngrx effect changes

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -5,4 +5,5 @@ import { Book } from "../models/book";
 export const AddBook = createAction('[Book] Add Book', props<Book>());
 export const RemoveBook = createAction('[Book] Remove Book', props<{bookId: string}>());
 
-
+export const AddBookSuccess = createAction('[Book] Add Book Success', props<Book>());
+export const AddBookFailure = createAction('[Book] Add Book Failure', props<{error: any}>());
--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,7 +1,8 @@
 import { createReducer } from "@ngrx/store";
 import { on } from "@ngrx/store";
-import { AddBook, RemoveBook } from "./book.actions";
+import { AddBook, AddBookFailure, AddBookSuccess, RemoveBook } from "./book.actions";
 import { Book } from "../models/book";
+import { state } from "@angular/animations";
 
 export const initialState: Book[] = [];
     //removed ReadonlyArray<Book[]>, meaning initialized initiateState to Book array
@@ -9,7 +10,16 @@ export const initialState: Book[] = [];
 export const BookReducer = createReducer(
     initialState,
     on(AddBook, (state, {id, title, author}) => [...state, {id, title, author}]),
-        //we will take current state and add new book at end of current state (book array) 
+        //we will take current state and add new book at end of current state (book array)
+    
+    //changes with @NgRx/effects
+    on(AddBook, (state) => { return state }),    
+    on(AddBookSuccess, (state, {id, title, author}) => [...state, {id, title, author}]),
+    on(AddBookFailure, (state, {error}) => {
+        console.error(error);
+        return state;
+    }),
+
     on(RemoveBook, (state, {bookId}) => state.filter(book => book.id))
         //we will take current state and book id of book to be deleted 
         // and return state and filter which will return new array ie. new state without book we deleted 
